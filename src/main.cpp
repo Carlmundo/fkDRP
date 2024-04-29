8cbd0e9d9933c210d45ca258d9c3639160213c08
Carl Norton <Carlmundo@users.noreply.github.com>
2024-04-29 22:51:26 +0100

    Removed code superseded by new fk Loader

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,11 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <tchar.h>
-#include "PEInfo.h"
 
 HMODULE self_module;
 HANDLE main_thread;
 std::string status;
 
-std::string getGameVersion(DWORD timeDateStamp){
-    switch (timeDateStamp) {
-        case 0x3528DAFA: return "BR";
-        case 0x3528DCB1: return "EU";
-        case 0x3528DB52: return "GE";
-        case 0x3528DA98: return "NA";
-        case 0x3528DBDA: return "SA";
-        case 0x3587BE19: return "TRY";
-    }
-    return "NONE";
-}
 bool IsProcessRunning(const TCHAR* const executableName) {
     PROCESSENTRY32 entry;
     entry.dwSize = sizeof(PROCESSENTRY32);
@@ -41,38 +29,30 @@ bool IsProcessRunning(const TCHAR* const executableName) {
     return false;
 }
 int main(){
-    PEInfo pe;
-    int tds = pe.FH->TimeDateStamp;
-    std::string version = getGameVersion(tds);
-    if (version != "NONE") {
-        discordInit();
-        status = "";
-        while (true) {
-            int64_t start_time;
-            std::string status_temp;
-            if (IsProcessRunning("worms2.exe")) {
-                status_temp = "Playing a game";
+    discordInit();
+    status = "";
+    while (true) {
+        int64_t start_time;
+        std::string status_temp;
+        if (IsProcessRunning("worms2.exe")) {
+            status_temp = "Playing a game";
+        }
+        else {
+            status_temp = "In menus";
+        }
+        if (status_temp != status) {
+            status = status_temp;
+            if (status == "Playing a game") {
+                start_time = time(0);
             }
             else {
-                status_temp = "In menus";
+                start_time = NULL;
             }
-            if (status_temp != status) {
-                status = status_temp;
-                if (status == "Playing a game") {
-                    start_time = time(0);
-                }
-                else {
-                    start_time = NULL;
-                }
-                updateDiscordPresence(start_time, NULL, status.c_str(), "cover", "", NULL, NULL);
-            }
-            Sleep(1000);
+            updateDiscordPresence(start_time, NULL, status.c_str(), "cover", "", NULL, NULL);
         }
-        return 0;
+        Sleep(1000);
     }
-    else {
-        return 0;
-    }
+    return 0;
 }
 
 bool APIENTRY DllMain(HINSTANCE hDLLInst, DWORD fdwReason, LPVOID lpvReserved) {
